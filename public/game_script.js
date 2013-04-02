221d0a889285c16b0ef848013aee3e1bcab3de7c
Nemrow <nemrowj@gmail.com>
2013-04-02 16:54:04 -0700

    foundation setup

--- public/game_script.js
+++ public/game_script.js
@@ -1,1 +1,20 @@
-alert('s')
+$(document).ready(function(){
+	problems_array = [
+		["array = [1,2,3,4,5]", "array[2]", 3],
+		[2,2],
+		[3,3]
+	]
+	$('.array_placer').text(problems_array[0][0])
+	$('.problem_placer').text(problems_array[0][1])
+	$('.answer_placer').text(problems_array[0][2])
+	current_array_box = 2
+	$('.array_input').live('keypress', function(e) { 
+		var code = (e.keyCode ? e.keyCode : e.which);
+ 		if(code == 13) { 
+ 			$('.array_input_text_boxes').append('<p>,</p><input class="array_input array_input_'+current_array_box+'" type="text">');
+ 			$('.array_input_'+current_array_box).focus();
+ 			current_array_box += 1
+ 			$('.num_of_elements').text($('.array_input').length)
+ 		}
+	});
+})
